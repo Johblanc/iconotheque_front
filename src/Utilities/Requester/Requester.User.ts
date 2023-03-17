4ce98e0a08834885c6d6ee8825a3e3aebb9111f5
Johblanc <79146321+Johblanc@users.noreply.github.com>
2023-03-17 14:57:53 +0100

    Feat : UserRequester.logIn

---
+++ src/Utilities/Requester/Requester.User.ts
@@ -0,0 +1,34 @@
+import { RequestMethods } from "../Types/RequestMethod";
+import { TResponse } from "../Types/TResponse";
+import { TUser } from "../Types/User.type";
+import { RequesterBase } from "./RequesterBase";
+
+/**
+ * Permet les requêtes pour la route users
+ * 
+ * * **logIn** : Requête pour log in un utilisateur
+ * 
+ * @version v1
+ */
+export class UserRequester extends RequesterBase {
+
+
+  /**
+   * Requête pour log in un utilisateur
+   * 
+   * @param body  Le nom et le mot de passe de l'utilisateur
+   * @returns     la réponse compléte de la requête
+   * 
+   * @version v1
+   */
+  static async logIn(body : { name: string, password: string}) : Promise<TResponse<TUser>>
+  {
+    const responce = await UserRequester.base<TUser>(
+      "users/login",
+      RequestMethods.POST,
+      [],
+      body
+    )
+    return responce
+  }
+}
