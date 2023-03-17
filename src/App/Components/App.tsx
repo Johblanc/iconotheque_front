34f2f717bf4db12328a35775fddeeb8614a2e4d5
Johblanc <79146321+Johblanc@users.noreply.github.com>
2023-03-17 09:36:02 +0100

    Feat : Page.context

--- src/App/Components/App.tsx
+++ src/App/Components/App.tsx
@@ -1,9 +1,25 @@
 
+import { useState } from 'react';
+import { LogInPage } from '../../LogIn/Components/LogInPage';
+import { PageContext } from '../../Utilities/Contexts/Page.context';
 import '../Style/App.style.css';
 
+
+/**
+ * Gestionnaire d'affichage des pages de l'application
+ * 
+ * @version v1
+ */
 function App() {
+
+  /** la page en cours d'affichage (lié au PageContext) */
+  const [page , setPage] = useState(<LogInPage/>)
+
   return (
     <div className="App">
+      <PageContext.Provider value={{page , setPage}} >
+      {page}
+      </PageContext.Provider>
     </div>
   );
 }
---
+++ src/LogIn/Components/LogInPage.tsx
@@ -0,0 +1,11 @@
+
+
+/**
+ * Gestion du Log In et du Register
+ * 
+ * @version v1
+ */
+export function LogInPage()
+{
+  return <>Login Page</>
+}
