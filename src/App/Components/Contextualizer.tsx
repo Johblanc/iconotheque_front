f8f0d17711c82438245bee68b36dd56f4781dbad
Johblanc <79146321+Johblanc@users.noreply.github.com>
2023-03-19 11:47:37 +0100

    Feat : Contextualiser

---
+++ src/App/Components/Contextualizer.tsx
@@ -0,0 +1,78 @@
+import { useEffect, useState } from "react";
+import { DEFAULT_USER } from "../../Utilities/Constants/User.defaut";
+import { PageContext } from "../../Utilities/Contexts/Page.context";
+import { PathPrivateContext } from "../../Utilities/Contexts/PathPrivate.context";
+import { PathPublicContext } from "../../Utilities/Contexts/PathPublic.context";
+import { UserContext } from "../../Utilities/Contexts/User.context";
+import { Requester } from "../../Utilities/Requester/Requester";
+import { TPath } from "../../Utilities/Types/Path.type";
+import "../Style/App.style.css";
+
+
+
+/**
+ * Un conteneur avec tous les contextes
+ * 
+ * * user
+ * * nextPage
+ * * icônes publiques
+ * * icônes privés
+ * 
+ * @version v1
+ */
+export function Contextualizer(props:{children : JSX.Element | JSX.Element[] | null }){
+  /** la page en cours d'affichage (lié au PageContext) */
+  const [nextPage, setNextPage] = useState(<></>);
+
+
+  /** l'utilisateur en cours d'utilisation */
+  const [user, setUser] = useState(DEFAULT_USER);
+
+  /** liste des paths publiques */
+  const [pathPublic, setPathPublic] = useState<TPath[]>([]);
+
+  /** liste des paths privés */
+  const [pathPrivate, setPathPrivate] = useState<TPath[]>([]);
+
+  /** Récupération des paths */
+  useEffect(() => {
+    const fetchPublics = async () => {
+      const response = await Requester.path.getPublics(user.token);
+      if (response.data) {
+        setPathPublic(response.data);
+      }
+    };
+
+    const fetchPrivates = async () => {
+      const response = await Requester.path.getPrivates(user.token);
+      if (response.data) {
+        setPathPrivate(response.data);
+      }
+    };
+    if (user.access > 0) {
+      fetchPublics();
+    } else {
+      setPathPublic([]);
+    }
+    if (user.access > 1) {
+      fetchPrivates();
+    } else {
+      setPathPrivate([]);
+    }
+  }, [user]);
+
+  return (
+    <div className="App">
+      <PageContext.Provider value={{ page: nextPage, setPage: setNextPage }}>
+        <UserContext.Provider value={{ user, setUser }}>
+          <PathPublicContext.Provider value={{ pathPublic, setPathPublic }}>
+            <PathPrivateContext.Provider value={{ pathPrivate, setPathPrivate }} >
+              {props.children}
+
+            </PathPrivateContext.Provider>
+          </PathPublicContext.Provider>
+        </UserContext.Provider>
+      </PageContext.Provider>
+    </div>
+  );
+}
