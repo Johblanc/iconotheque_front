f2854ddd9f246639c1992f203546bd2181e3192a
Johblanc <79146321+Johblanc@users.noreply.github.com>
2023-04-13 11:57:10 +0200

    Feat : Nav on LogIn

--- src/User/Pages/UserLogInPage.tsx
+++ src/User/Pages/UserLogInPage.tsx
@@ -1,5 +1,6 @@
 
 import { AppHeader } from "../../App/Components/AppHeader";
+import { AppNav } from "../../App/Components/AppNav";
 import { APP_STYLE } from "../../App/Style/App.bootstrap.style";
 import { LogInBox } from "../Components/LogInBox";
 import { SignInBox } from "../Components/SignInBox";
@@ -14,6 +15,7 @@ export function UserLogInPage(): JSX.Element {
   return (
     <div className={APP_STYLE.APP.PAGE}>
       <AppHeader />
+      <AppNav actif={"logIn"}/>
       <div className={APP_STYLE.USER.LOGIN.CADRE_A}>
         <div className={APP_STYLE.USER.LOGIN.CADRE_B}>
           <LogInBox />
