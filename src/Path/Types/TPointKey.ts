f635a8d6ee8599b7c73a467c3aa52bc1f6d48391
Johblanc <79146321+Johblanc@users.noreply.github.com>
2023-04-19 21:13:56 +0200

    Feat : TPointKey

---
+++ src/Path/Types/TPointKey.ts
@@ -0,0 +1,8 @@
+
+
+/**
+ * Liste des propriétés pour le tracé
+ * 
+ * @version v2
+ */
+export type TPointKey = "dx1"|"dy1"|"dx2"|"dy2"|"x1"|"y1"|"x2"|"y2"|"rx"|"ry"|"angle"|"largeArcFlag"|"sweepFlag"|"dx"|"dy"|"x"|"y" ;
