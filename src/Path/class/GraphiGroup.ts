20a8c469a210db6c4ad4d067d9d89798357f0d76
Johblanc <79146321+Johblanc@users.noreply.github.com>
2023-04-20 00:34:05 +0200

    Feat : GraphiGroup as string

--- src/Path/class/GraphiGroup.ts
+++ src/Path/class/GraphiGroup.ts
@@ -1,4 +1,5 @@
 import { TGraphiGroup } from "../Types/TGraphiGroup";
+import { GraphiFormOptions } from "./GraphiFormOptions";
 import { GraphiPoint } from "./GraphiPoint";
 
 
@@ -26,5 +27,52 @@ export class GraphiGroup {
       points : this.points.map(item => item.copy),
       isClose : this.isClose
     })
+  }
+  
+  /**
+   * Convertion de GraphiGroups en string
+   * 
+   * @version v2
+   */
+
+  static fromGraphiToString(
+    data : GraphiGroup[],
+    formOptions : GraphiFormOptions
+  )
+  {
+
+    return data.map(forme => {
+
+      let result = "M" ;
+
+      const newPoints = forme.points.map(item => formOptions.allOptionsGraphiPoint( item ))
+
+      if (forme.isClose)
+      {
+        result += ` ${newPoints[newPoints.length-1].x} ${newPoints[newPoints.length-1].y}` ;
+        result += `${newPoints.map((point, i, arr) => {
+          if (i === 0){
+            return ` C ${arr[arr.length-1].bx} ${arr[arr.length-1].by} ${point.ax} ${point.ay} ${point.x} ${point.y}`
+          }
+          return ` C ${arr[i-1].bx} ${arr[i-1].by} ${point.ax} ${point.ay} ${point.x} ${point.y}`
+        
+        }).join(" ")}` ;
+      }
+      else
+      {
+        result += ` ${newPoints[0].x} ${newPoints[0].y}` ;
+        result += `${newPoints.map((point, i, arr) => {
+          if (i === 0){
+            return ``
+          }
+          return ` C ${arr[i-1].bx} ${arr[i-1].by} ${point.ax} ${point.ay} ${point.x} ${point.y}`
+        }).join("")}` ;
+      }
+      return result
+    }).join(" ")
+  }
+
+  asString(formOptions : GraphiFormOptions) {
+    return GraphiGroup.fromGraphiToString([this],formOptions)
   }
 }
