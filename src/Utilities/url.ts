7fafae609e79b0eaa71ccf4cfaa1aaaedbc797ad
Johblanc <79146321+Johblanc@users.noreply.github.com>
2023-04-22 10:53:04 +0200

    Body : Base URL

--- src/Utilities/url.ts
+++ src/Utilities/url.ts
@@ -1,1 +1,4 @@
-export const BASE_URL = 'http://10.0.4.123:8000/api';
+
+export const BASE_URL = 'http://localhost:8000/api';
+//export const BASE_URL = 'http://192.168.1.83:8000/api';
+//export const BASE_URL = 'http://10.0.4.123:8000/api';
